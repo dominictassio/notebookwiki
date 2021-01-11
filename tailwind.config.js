f80d2bd14a397da2562cd50a42ffcbbb8e1948c9
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-11 00:41:14 -0600

    Configure tailwind with paper breakpoint that has a min-width of 8in

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      paper: "8in",
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    ({ addComponents }) =>
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen paper": {
+            maxWidth: "8in",
+          },
+        },
+      }),
+  ],
 };
