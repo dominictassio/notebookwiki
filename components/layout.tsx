20862c2e07b76adaf067af9e804fffa0f594b9d7
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-11 00:44:27 -0600

    Add emoji favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,7 +12,11 @@ const Layout = ({
 }) => (
   <>
     <Head>
-      <title>{title}</title>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>"
+      ></link>
+      <title>notebookwiki - {title}</title>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
     <header>
