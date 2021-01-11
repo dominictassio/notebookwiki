4ee34894716297745df4f426ae17ce725ee17541
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-11 01:28:48 -0600

    Add content to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
 import Layout from "../components/layout";
+import NotebookPage from "../components/notebook-page";
 
 const IndexPage = () => (
   <Layout title="Home">
-    <span>Home</span>
+    <div className="container mx-auto">
+      <NotebookPage>
+        <h1>Batman</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          incidunt ullam doloribus iure sit sequi fugiat in, itaque excepturi
+          debitis ducimus autem ipsa ipsam dolores. Ipsam quam optio, alias
+          quibusdam qui tempora dicta unde voluptate, beatae reiciendis
+          incidunt, officiis assumenda!
+        </p>
+        <p>
+          Qui ipsa sapiente, officia, earum illum illo quisquam ipsam fuga sunt
+          ab quod optio. Cupiditate, quis. Temporibus ut ea quaerat cumque
+          minima ullam repudiandae incidunt voluptatem vero cupiditate, tempore
+          similique at? Magnam praesentium assumenda minima enim rem neque
+          debitis quidem?
+        </p>
+        <p>
+          Enim quaerat facilis fuga est aspernatur error consequatur sunt
+          quisquam in aut voluptatibus deserunt animi corporis perspiciatis
+          repellat reiciendis impedit suscipit deleniti, nam id provident quas
+          laboriosam! Unde voluptates repellat ipsam esse totam dolorum pariatur
+          consequatur. Totam non amet est!
+        </p>
+        <p>
+          Vero harum adipisci maxime quod, ipsa voluptates voluptatum sequi
+          quos, itaque est non culpa minima deserunt suscipit eaque. Qui dicta,
+          pariatur autem esse dignissimos suscipit magni quidem deserunt?
+          Explicabo, dolor. Itaque hic porro minima? Ullam aperiam obcaecati
+          totam repudiandae at.
+        </p>
+      </NotebookPage>
+    </div>
   </Layout>
 );
 
