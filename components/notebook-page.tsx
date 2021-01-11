3cb664d85b0e60ef7524b17f8bcd767887ec7460
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-11 00:46:46 -0600

    Set the NotebookPage component to have a fixed height

--- components/notebook-page.tsx
+++ components/notebook-page.tsx
@@ -3,23 +3,32 @@ import "@fontsource/kalam";
 
 const NotebookPage = ({ children }: { children?: ReactNode }) => (
   <>
-    <div className="paper mx-auto my-2 relative shadow-md">
+    <div className="paper my-2 relative shadow-md">
       <div className="lines">
-        <div className="content pb-0">{children}</div>
+        <div className="content overflow-hidden relative pb-0">{children}</div>
       </div>
     </div>
     <style jsx>{`
       .paper {
         background-color: ghostwhite;
-        min-height: 10.5in;
+        height: 10.5in;
         padding-bottom: 0.28125in;
         padding-top: 1.21875in;
         width: 8in;
-        &:before {
+        &::after {
           background-color: rgba(255, 0, 0, 0.6);
           content: "";
           height: 100%;
           left: calc(1.25in - 1px);
+          position: absolute;
+          top: 0;
+          width: 1px;
+        }
+        &::before {
+          background-color: rgba(255, 0, 0, 0.1);
+          content: "";
+          height: 100%;
+          right: calc(1.25in - 1px);
           position: absolute;
           top: 0;
           width: 1px;
@@ -38,9 +47,13 @@ const NotebookPage = ({ children }: { children?: ReactNode }) => (
       .content {
         font-family: "Kalam", cursive;
         font-size: 0.25in;
+        height: 100%;
         line-height: 0.28125in;
         margin: 0 1.25in 0.28125in 1.25in;
         padding: 6px;
+        :global(h1) {
+          text-align: center;
+        }
         :global(p) {
           text-indent: 0.5in;
         }
