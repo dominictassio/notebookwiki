50ee29fdbe2feb183ac45799645c28436855fd46
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-12 03:28:50 -0600

    Change the index page's content and get rid of the container around the notebook page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,84 @@
-import Container from "../components/container";
 import Layout from "../components/layout";
 import NotebookPage from "../components/notebook-page";
 
 const IndexPage = () => (
   <Layout title="Home">
-    <Container>
-      <NotebookPage>
-        <h1>Title</h1>
+    <div className="flex justify-center space-x-6">
+      <NotebookPage title="Title" editable>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          incidunt ullam doloribus iure sit sequi fugiat in, itaque excepturi
-          debitis ducimus autem ipsa ipsam dolores. Ipsam quam optio, alias
-          quibusdam qui tempora dicta unde voluptate, beatae reiciendis
-          incidunt, officiis assumenda!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+          maxime nihil dolor vero quis iure vitae tempore laudantium officia
+          perferendis cumque possimus mollitia laboriosam, odit non quibusdam at
+          illum corporis accusamus! Ratione ea deserunt pariatur doloremque
+          eveniet numquam rerum quasi modi eligendi asperiores cupiditate, ipsum
+          perspiciatis veritatis sit adipisci. Doloribus deleniti quasi in
+          praesentium enim consequuntur quis optio voluptatum ut rerum tempora
+          error natus quia sapiente, commodi dolores, iure reprehenderit,
+          temporibus a iusto laboriosam. Ducimus consequatur suscipit ullam
+          voluptatem fuga.
         </p>
         <p>
-          Qui ipsa sapiente, officia, earum illum illo quisquam ipsam fuga sunt
-          ab quod optio. Cupiditate, quis. Temporibus ut ea quaerat cumque
-          minima ullam repudiandae incidunt voluptatem vero cupiditate, tempore
-          similique at? Magnam praesentium assumenda minima enim rem neque
-          debitis quidem?
+          Vero, cum modi unde deserunt amet maxime aspernatur similique soluta
+          quo laudantium obcaecati, voluptatibus asperiores sed fugiat possimus
+          quisquam suscipit, necessitatibus eligendi quibusdam laboriosam! Alias
+          quod iusto aut, odit vero deleniti sunt, voluptatem quae doloribus
+          amet praesentium. Provident, vitae ab ex nisi adipisci architecto
+          accusamus eligendi at neque nobis asperiores atque suscipit illo!
+          Voluptatum nobis magni facere repudiandae quam molestiae iusto nulla
+          dolor a dicta maxime soluta dolore facilis recusandae repellendus
+          voluptate, deserunt suscipit placeat voluptates quibusdam accusantium
+          libero nisi.
         </p>
         <p>
-          Enim quaerat facilis fuga est aspernatur error consequatur sunt
-          quisquam in aut voluptatibus deserunt animi corporis perspiciatis
-          repellat reiciendis impedit suscipit deleniti, nam id provident quas
-          laboriosam! Unde voluptates repellat ipsam esse totam dolorum pariatur
-          consequatur. Totam non amet est!
+          Eum aspernatur dignissimos sapiente, beatae praesentium, at asperiores
+          nam temporibus adipisci numquam, ipsa unde autem dicta accusantium.
+          Aspernatur aperiam voluptatem a exercitationem quasi ex nesciunt
+          consequatur quo quisquam iure dicta, commodi reprehenderit cumque
+          veniam laudantium iste. Ex quis, laboriosam unde nostrum nulla quo
+          commodi dolor delectus totam accusantium animi amet hic quos
+          reprehenderit modi voluptates cupiditate sit similique voluptate ut
+          perferendis dolore excepturi? Qui, eius. Neque voluptate nisi quam
+          dolores odio delectus consequatur nam. Nam pariatur adipisci sequi
+          iusto tenetur!
         </p>
         <p>
-          Vero harum adipisci maxime quod, ipsa voluptates voluptatum sequi
-          quos, itaque est non culpa minima deserunt suscipit eaque. Qui dicta,
-          pariatur autem esse dignissimos suscipit magni quidem deserunt?
-          Explicabo, dolor. Itaque hic porro minima? Ullam aperiam obcaecati
-          totam repudiandae at.
+          Velit numquam adipisci magnam quis totam quisquam nulla, sapiente
+          accusantium facilis reiciendis perferendis, itaque provident ipsam
+          nisi, autem impedit beatae labore dolores deleniti similique ea!
+          Aliquam laboriosam cum culpa nemo ipsa adipisci, aliquid temporibus
+          expedita natus tempore commodi, quibusdam suscipit dolores cumque
+          nulla assumenda dolor iusto officiis ex quisquam quae ipsam voluptatum
+          reprehenderit. In commodi, nostrum adipisci aliquid a nam! Consequatur
+          optio nisi mollitia at. Quo consectetur consequatur minus dolore
+          laboriosam dolor, adipisci, earum libero deserunt eius mollitia facere
+          ratione.
+        </p>
+        <p>
+          Quia autem repudiandae tenetur, quod corrupti sapiente, nihil nobis ex
+          iusto voluptates laborum? Et, repudiandae facilis. Consequatur,
+          aliquid impedit porro eius cumque nesciunt quidem voluptatibus
+          perferendis sed modi doloribus laboriosam soluta quo odio saepe
+          eveniet animi maiores? Recusandae fuga ullam iusto eius assumenda at
+          unde ea accusantium accusamus autem aperiam laudantium consectetur
+          inventore, corporis excepturi a aliquam voluptatibus. Magnam, sint.
+          Perferendis iste, ea labore impedit maiores, nisi molestiae quis
+          veniam ad doloremque reiciendis neque ducimus eum cumque adipisci, vel
+          explicabo.
+        </p>
+        <p>
+          Possimus, nisi magni iste porro doloremque blanditiis doloribus
+          cupiditate expedita atque. Vero reprehenderit praesentium inventore
+          ducimus veniam sit explicabo accusantium fuga ut non quos odio eveniet
+          perferendis ratione exercitationem a molestias nemo aliquid sunt porro
+          mollitia, eligendi culpa. Pariatur, rem consequuntur harum iusto velit
+          ducimus facere? Totam, eos adipisci distinctio aspernatur deserunt
+          quis a repudiandae minima nostrum, vero itaque architecto id maxime
+          non, esse porro quaerat atque? Nostrum quam adipisci quasi
+          consequuntur labore perspiciatis illo, voluptatibus ab accusamus,
+          porro officiis.
         </p>
       </NotebookPage>
-    </Container>
+    </div>
   </Layout>
 );
 
