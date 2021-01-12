9c6f1972f5c979acb57fdef084227b12db65ad6d
Dominic Tassio <dominic.tassio@gmail.com>
2021-01-11 18:32:59 -0600

    Set the page charset to utf-8

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,6 +17,7 @@ const Layout = ({
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>"
       ></link>
       <title>notebookwiki - {title}</title>
+      <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
     <header>
